a1469e8e769b4d277d7fdcf3894b93b45d1bc0c3
nic116seitz <25190512+nic116seitz@users.noreply.github.com>
2025-06-13 10:01:21 -0700

    Exercise Completed Nothing to add

--- 04_mathEquations/mathEquations.js
+++ 04_mathEquations/mathEquations.js
@@ -1,9 +1,9 @@
 const a = 1 - 1 // Freebie!!! This is the answer to "one minus one"
-const b = "one plus eight" // One plus eight
-const c = "22 times three" // 22 * three
-const d = "the *remainder* of 5/4" // remainder of 5/4
-const e = "the variable 'b' minus 17" // the variable b - 3 
-const f = "the sum of the previous five variables" // the sume of previous five variables
+const b = 1 + 8 // One plus eight
+const c = 22 * 3 // 22 * three
+const d = 5 % 4 // remainder of 5/4
+const e = b - 3 // the variable b - 3 
+const f = a + b + c + d + e // the sume of previous five variables
 
 // Do not edit below this line
 module.exports = {a, b, c, d, e, f}
